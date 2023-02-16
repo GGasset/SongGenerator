f815ece7fe1b8aadbed0199eaba7f7120d3c80c4
Germán Gasset <gassetgerman@gmail.com>
2023-02-16 22:31:58 +0100

    made setup.py easier to debug

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 import os
+from time import sleep
 import pandas as pd
 import sys
 
 def main():
     if sys.argv.count() < 2:
         print('Usage: python setup.py songs_csv_input_path songs_csv_output_path')
+        sleep(1)
         quit(1)
     songs_csv_from_path = sys.argv[0]
     songs_csv_to_path = sys.argv[1]
 
     if os.path.isfile(songs_csv_to_path):
         print('No need for setup')
+        sleep(1)
         return
 
     if not os.path.isfile(songs_csv_from_path):
         print('Original Songs.csv file missing, you need to clone submodules too.')
+        sleep(1)
         quit(1)
 
     songs_csv = pd.read_csv(songs_csv_from_path)
